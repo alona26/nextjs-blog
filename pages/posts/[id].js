ab15f052b29905520b192d7ecc9233d5d9b7b0d3
Alona Rozner <alonarozner4@gamil.com>
2023-12-25 15:22:22 +0200

    Fix Error

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,5 @@
 import Layout from "../components/layout";
 import Head from "next/head";
-import Link from "next/link";
 import Date from "../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
